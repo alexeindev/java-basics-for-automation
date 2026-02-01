ea723df89cab3b2a55f556e35ff54bd5f54a7e98
Alejandra Lopez <alejandralopez8212@gmail.com>
2026-02-01 15:34:43 -0500

    Add Browser enum class and it's methods

--- src/main/java/enums/browserfactory/Browser.java
+++ src/main/java/enums/browserfactory/Browser.java
@@ -1,4 +1,48 @@
 package enums.browserfactory;
 
-public class Browser {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+public enum Browser {
+    CHROME, FIREFOX, EDGE;
+
+
+    public WebDriver createDriver() {
+        WebDriver driver = switch (this) {
+            case CHROME -> new ChromeDriver(getChromeOptions());
+            case FIREFOX -> new FirefoxDriver(getFirefoxOptions());
+            case EDGE -> new EdgeDriver(getEdgeOptions());
+        };
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--start-maximized");
+        return options;
+    }
+
+    private FirefoxOptions getFirefoxOptions() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.addPreference("dom.webdriver.enabled", false);
+        options.addArguments("--width=1920");
+        return options;
+    }
+
+    private EdgeOptions getEdgeOptions() {
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--inprivate");
+        options.addArguments("--start-maximized");
+        return options;
+    }
+
 }
